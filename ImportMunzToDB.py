95749c6b647c0570943c011cbad25ada0e06d6d2
Doodleman360 <julislo123@gmail.com>
2020-09-29 22:17:21 -0600

    setup for new import script

---
+++ ImportMunzToDB.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import mariadb
+import sys
+
+auth = json.load(open('credentials.json'))
+munzAuth = json.load(open('munzcreds.json'))
+# keys
+user = auth["user"]
+passwd = auth["pass"]
+BearerToken = munzAuth["data"]["token"]["access_token"]
+
+try:
+    conn = mariadb.connect(
+        user=user,
+        password=passwd,
+        host="localhost",
+        port=3306,
+        database="munz"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
